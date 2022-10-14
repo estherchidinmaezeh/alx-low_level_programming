8abb187f8f51604f80c0497c736ba1711c9b2cd4
esther <estherezeh88@gmail.com>
2022-10-14 09:02:36 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Bnum;
+
+	for (Bnum = 0; Bnum < 10; Bnum++)
+		putchar(Bnum);
+	putchar('\n');
+	return (0);
+}
