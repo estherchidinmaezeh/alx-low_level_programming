98a82284eacbf71a4e1605877dfa81ee25a06383
esther <estherezeh88@gmail.com>
2022-10-14 03:18:03 -0700

    ALX LOW LEVEL PROGRAMMING

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,13 @@ int main(void)
 	char Char;
 	int Int;
 	long int LInt;
-	long long int LLI;
+	long long int LL;
 	float Float;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(Char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(Int));
 	printf("Size of an long int: %lu byte(s)\n", (unsigned long) sizeof(LInt));
-	printf("Size of an long long int: %lu byte(s)\n", (unsigned long) sizeof(LLI));
+	printf("Size of an long long int: %lu byte(s)\n", (unsigned long) sizeof(LL));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(Float));
 	return (0);
 }
